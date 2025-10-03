2acb0a2cf83df2d03327038b3a9872aa4149551d
6931919 <ka01710@surrey.ac.uk>
2025-10-03 12:39:40 +0100

    Consolidation task 1

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-// Get a reference to the button element
 var button = document.getElementById("colorButton");
-// Function to change the button color
-function changeColor() {
-// Generate a random color (hexadecimal)
-var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-// Set the button's background color to the random color
-button.style.backgroundColor = randomColor;
-}
-// Add a click event listener to the button
-button.addEventListener("click", changeColor);
 
 var text = document.getElementById("input1");
 
-function print_text(){
-    console.log(text.value);
-}
-var choice = false;
+var choice = true;
 
-
-function hide_button() {
-    if (choice == true){
-        document.getElementById("input1").style.display = "none";
-    }
-    else {
-        document.getElementById("input1").style.display = "inline";
-    }
-    choice = !choice;
+function output_text() {
+    window.alert("Entered " + text.value)
 }
 
-button.addEventListener("click", hide_button);
+button.addEventListener("click", output_text);
