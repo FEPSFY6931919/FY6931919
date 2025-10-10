47514bf98b765c3d3bc06681342f1bb415d63a5f
6931919 <ka01710@surrey.ac.uk>
2025-10-10 08:08:28 +0100

    Consolidation task 2

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ google.charts.setOnLoadCallback(drawChart);
 
 
 function drawChart() {
-    var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Cities');
-        data.addColumn('number', 'Population');
-        data.addRows([
-            ['London', 9648000],
-            ['Birmingham', 1153900],
-            ['Edinburgh', 554000],
+    var data = new google.visualization.arrayToDataTable([
+        ['Year', 'London', 'Birmingham', 'Edinburgh'],
+        ['2023', 9648000, 1153900, 554000],
+        ['2021', 9426000, 1142500, 543000],
+        ['2019', 9177000, 1141800, 531000],
+        ['2017', 8916000, 1137100, 519000],
+        ['2015', 8661000, 1113000, 507000],
+        ['2013', 8414000, 1092200, 495000]
         ]);
         var options = {
         title: 'Population of different cities in 2024',
         hAxis: { title: 'Cities' },
         vAxis: { title: 'Population' },
-        colors: ['red']
+        colors: ['red', 'green', 'blue']
     };
     
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
     
     chart.draw(data, options);
 }
